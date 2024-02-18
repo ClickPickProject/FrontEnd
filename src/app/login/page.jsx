06fdc23e6e85837aab2f295e9df83d43213c1c85
Elbyss <elysiabyss@gmail.com>
2024-02-18 13:57:55 +0900

    Design: 스타일 일부 수정

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,38 +1,35 @@
 'use client';
-import HomeNavbar from '@/components/Home/HomeNavbar';
 import { MdOutlineMailOutline } from 'react-icons/md';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import Image from 'next/image';
 import Link from 'next/link';
 export default function LoginPage() {
-  const defaultInputStyle = 'rounded-md outline-none h-[50px] text-lg pl-8';
-
+  const defaultInputStyle = 'rounded-lg outline-none h-[45px] pl-7';
   return (
     <>
       <div className='h-[100dvh] bg-[#fdf4f5]'>
-        <HomeNavbar />
         <section className='absolute left-1/2 top-1/2 flex h-[550px] w-[500px] -translate-x-1/2 -translate-y-1/2 flex-col  rounded-2xl  bg-pink-200  shadow-[1px_1px_200px_1px] shadow-pink-200'>
-          <figure className='mx-auto p-8'>
+          <figure className='mx-auto p-6'>
             <Image src='/Images/clickpick_icon.png' alt='' width={52} height={52} />
           </figure>
-          <h2 className='mx-auto mb-8 text-4xl font-bold'>클릭픽 로그인</h2>
+          <h2 className='mx-auto mb-8 text-2xl font-bold'>클릭픽 로그인</h2>
           <form className='flex w-full flex-col items-center justify-center gap-8'>
-            <div className='relative flex w-[400px] flex-col'>
+            <div className='relative flex w-[350px] flex-col'>
               <MdOutlineMailOutline
-                size={24}
+                size={20}
                 className='absolute ml-1 flex h-full items-center justify-center opacity-50'
               />
               <input placeholder='아이디' type='text' id='id' required className={`${defaultInputStyle}`} />
             </div>
-            <div className='relative flex w-[400px] flex-col'>
+            <div className='relative flex w-[350px] flex-col'>
               <RiLockPasswordLine
-                size={24}
+                size={20}
                 className='absolute ml-1 flex h-full items-center justify-center opacity-50'
               />
               <input placeholder='비밀번호' type='password' id='password' required className={`${defaultInputStyle}`} />
             </div>
             <div className='flex justify-center'>
-              <button className='flex h-[50px] w-[400px]  items-center justify-center rounded-lg bg-pink-400 text-2xl font-semibold text-white transition-all hover:bg-pink-500 hover:text-white'>
+              <button className='flex h-[50px] w-[350px]  items-center justify-center rounded-lg bg-pink-400 text-2xl font-semibold text-white transition-all hover:bg-pink-500 hover:text-white'>
                 로그인
               </button>
             </div>
@@ -44,7 +41,7 @@ export default function LoginPage() {
                 </Link>
               </div>
             </div>
-            <div>소셜 로그인</div>
+            {/* <div>소셜 로그인</div> */}
           </form>
         </section>
       </div>
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,38 +1,44 @@
-import HomeNavbar from '@/components/Home/HomeNavbar';
+'use client';
 import { MdOutlineMailOutline } from 'react-icons/md';
+import { SiNamecheap } from 'react-icons/si';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import Image from 'next/image';
 import Link from 'next/link';
 export default function SignUpPage() {
-  const defaultInputStyle = 'rounded-md outline-none h-[50px] text-lg pl-8';
+  const defaultInputStyle = 'rounded-lg outline-none h-[45px] pl-7';
 
   return (
     <>
       <div className='h-[100dvh] bg-[#fdf4f5]'>
-        <HomeNavbar />
         <section className='absolute left-1/2 top-1/2 flex h-[700px] w-[500px] -translate-x-1/2 -translate-y-1/2 flex-col  rounded-2xl  bg-pink-200  shadow-[1px_1px_200px_1px] shadow-pink-200'>
-          <figure className='mx-auto p-8'>
+          <figure className='mx-auto p-6'>
             <Image src='/Images/clickpick_icon.png' alt='' width={52} height={52} />
           </figure>
-          <h2 className='mx-auto mb-8 text-4xl font-bold'>클릭픽 회원가입</h2>
-          <form className='flex w-full flex-col items-center justify-center gap-12'>
-            <div className='relative flex w-[400px] flex-col'>
+          <h2 className='mx-auto mb-8 text-2xl font-bold'>클릭픽 회원가입</h2>
+          <form className='j flex w-full flex-col items-center justify-center gap-4'>
+            <div className='relative flex w-[350px] flex-col'>
               <MdOutlineMailOutline
-                size={24}
-                className='absolute ml-1 flex h-full items-center justify-center opacity-50'
+                size={20}
+                className='absolute ml-1 flex h-full items-center justify-center  opacity-50'
               />
               <input placeholder='아이디' type='text' id='id' required className={`${defaultInputStyle}`} />
             </div>
-            <div className='relative flex w-[400px] flex-col'>
+            {/* <div className='flex w-[350px] font-semibold text-red-600'>아이디는 4~12자 이내로 입력해주세요</div> */}
+            <div className='flex w-[350px] font-semibold text-blue-600'>사용할 수 있는 아이디입니다.</div>
+            <div className='relative flex w-[350px] flex-col'>
               <RiLockPasswordLine
-                size={24}
+                size={20}
                 className='absolute ml-1 flex h-full items-center justify-center opacity-50'
               />
               <input placeholder='비밀번호' type='password' id='password' required className={`${defaultInputStyle}`} />
             </div>
-            <div className='relative flex w-[400px] flex-col'>
+            {/* <div className='flex w-[350px] font-semibold text-red-600'>
+              영문과 숫자를 포함하여 6자 이상 입력해주세요
+            </div> */}
+            <div className='flex w-[350px] font-semibold text-blue-600'>사용할 수 있는 비밀번호입니다.</div>
+            <div className='relative flex w-[350px] flex-col'>
               <RiLockPasswordLine
-                size={24}
+                size={20}
                 className='absolute ml-1 flex h-full items-center justify-center opacity-50'
               />
               <input
@@ -43,20 +49,26 @@ export default function SignUpPage() {
                 className={`${defaultInputStyle}`}
               />
             </div>
+            {/* <div className='flex w-[350px] font-semibold text-red-600'>비밀번호가 서로 일치하지 않습니다.</div> */}
+            <div className='flex w-[350px] font-semibold text-blue-600'>비밀번호가 서로 일치합니다.</div>
+            <div className='relative flex w-[350px] flex-col'>
+              <SiNamecheap size={20} className='absolute ml-1 flex h-full items-center justify-center opacity-50' />
+              <input placeholder='닉네임' type='text' id='password' required className={`${defaultInputStyle}`} />
+            </div>
+            {/* <div className='flex w-[350px] font-semibold text-red-600'>이미 사용중인 닉네임입니다.</div> */}
+            <div className='flex w-[350px] font-semibold text-blue-500'>사용 가능한 닉네임입니다.</div>
             <div className='flex justify-center'>
-              <button className='flex h-[50px] w-[400px]  items-center justify-center rounded-lg bg-pink-400 text-2xl font-semibold text-white transition-all hover:bg-pink-500 hover:text-white'>
+              <button className='flex h-[45px] w-[350px]  items-center justify-center rounded-lg bg-pink-400 text-xl font-semibold text-white transition-all hover:bg-pink-500 hover:text-white'>
                 가입하기
               </button>
             </div>
             <div className='flex text-gray-600 '>
               이미 회원이신가요?
-              <div className='pl-2 font-semibold text-black'>
-                <Link href='/login' className=''>
-                  로그인
-                </Link>
-              </div>
+              <button className='pl-2 font-semibold text-black'>
+                <Link href='/login'>로그인</Link>
+              </button>
             </div>
-            <div>소셜 회원가입</div>
+            {/* <div>소셜 회원가입</div> */}
           </form>
         </section>
       </div>
