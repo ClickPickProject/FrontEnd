c2bbbe62d6baf5325ce7b3ce43c4fc3b318886df
Elbyss <elysiabyss@gmail.com>
2024-02-19 11:53:19 +0900

    로그인/회원가입 창 스타일링 작업 및  (#8)
    * Rename: 페이지 경로 수정
    
    * Fix: msw 코드 수정
    - msw version upgrade로 인한 코드 추가 작성
    - 전체 컴포넌트에 일괄 적용
    
    * Remove: MSW 삭제
    - nextjs app router와의 호환성 문제로 인해 삭제합니다.
    
    * Chore: 라이브러러 설치
    - axios
    - framer-motion
    - json-server
    
    * Design: 클릭픽 아이콘 이미지 추가
    
    * Design: 로그인/회원가입 스타일링
    
    * Design: 스타일 일부 수정
    
    * Chore: jsconfig 수정
    
    * Feat(route): 임시 라우트 추가

--- src/components/Home/HomeNavbar.jsx
+++ src/components/Home/HomeNavbar.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function HomeNavbar() {
   return (
@@ -6,14 +7,18 @@ export default function HomeNavbar() {
       <header className='h-[78px] bg-white'>
         <nav className='flex h-full items-center'>
           <figure className='ml-4'>
-            <Image src={'/Images/clickpick_logo.png'} alt='#' width={168} height={76} />
+            <Link alt='logo' href='/'>
+              <Image src={'/Images/clickpick_logo.png'} alt='#' width={168} height={76} />
+            </Link>
           </figure>
           <ul className='flex w-full justify-between gap-4 text-[20px] font-bold'>
-            <div className='m-auto flex gap-4'>
+            <div className='mx-auto flex gap-4'>
               <li className='border-b-2 border-pink-400 pb-1 text-pink-400'>장소찾기</li>
               <li>커뮤니티</li>
             </div>
-            <li className='mr-[50px]'>로그인</li>
+            <Link href='/login' className='mr-[50px]'>
+              로그인
+            </Link>
           </ul>
         </nav>
       </header>
