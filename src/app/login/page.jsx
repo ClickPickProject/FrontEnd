beeca577d180ff9af8e948be0761c96d3493924d
Jungmu <wjdan022@naver.com>
2024-02-20 15:09:30 +0900

    로그인 js 구현

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,11 +3,23 @@ import { MdOutlineMailOutline } from 'react-icons/md';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 export default function LoginPage() {
+  let [id, setId] = useState(''); // setState로 id 초기값 공백
+  let [pw, setPw] = useState(''); // setState로 password초기값 공백
+  const [button, setButton] = useState(true);
+  const goToMain = () => {
+    NavigationPreloadManager('/page');
+  };
   const defaultInputStyle = 'rounded-lg outline-none h-[45px] pl-7';
+  const realId = 'kiki@naver.com';
+  const realPw = '12345678';
+  function changeButton() {
+    id.includes('@') && pw.length >= 5 ? setButton(false) : setButton(true);
+  } // changeButton()함수는 id에 @가 포함되어있고 pw의 글자가 5글자 이상일때 조건식이 맞을때 false, 맞지 않을때 true로 견경해줌.
+
   return (
     <>
-      코딩으로 밥벌어먹고 살기 힘들다
       <div className='h-[100dvh] bg-[#fdf4f5]'>
         <section className='absolute left-1/2 top-1/2 flex h-[550px] w-[500px] -translate-x-1/2 -translate-y-1/2 flex-col  rounded-2xl  bg-pink-200  shadow-[1px_1px_200px_1px] shadow-pink-200'>
           <figure className='mx-auto p-6'>
@@ -20,17 +32,58 @@ export default function LoginPage() {
                 size={20}
                 className='absolute ml-1 flex h-full items-center justify-center opacity-50'
               />
-              <input placeholder='아이디' type='text' id='id' required className={`${defaultInputStyle}`} />
+              <input
+                placeholder='아이디'
+                type='text'
+                id='id'
+                required
+                className={`${defaultInputStyle}`}
+                onChange={(e) => {
+                  setId(e.target.value);
+                }}
+                onKeyUp={changeButton}
+              />
+              {/* //input에 입력되는 내용이 바뀔때마다 e.target.value의 값이 id로 담김 */}
             </div>
             <div className='relative flex w-[350px] flex-col'>
               <RiLockPasswordLine
                 size={20}
                 className='absolute ml-1 flex h-full items-center justify-center opacity-50'
               />
-              <input placeholder='비밀번호' type='password' id='password' required className={`${defaultInputStyle}`} />
+              <input
+                placeholder='비밀번호'
+                type='password'
+                id='password'
+                required
+                className={`${defaultInputStyle}`}
+                onChange={(e) => {
+                  setPw(e.target.value);
+                }}
+                onKeyUp={changeButton}
+              />
+              {/* input에 입력되는 내용이 바뀔때마다 e.target.value의 값이 pw로 담김 */}
             </div>
             <div className='flex justify-center'>
-              <button className='flex h-[50px] w-[350px]  items-center justify-center rounded-lg bg-pink-400 text-2xl font-semibold text-white transition-all hover:bg-pink-500 hover:text-white'>
+              <button
+                type='button'
+                disabled={button}
+                onClick={(e) => {
+                  if (realId == id) {
+                    if (realPw == pw) {
+                      e.stopPropagation();
+                      goToMain();
+                    }
+                  } else {
+                    alert('아이디 혹은 비밀번호가 일치하지 않습니다.');
+                  }
+                }}
+                // button의 값이 true일경우 disabled가 작동 false일 경우 disabled가 작동하지 않도록 disabled={button}을 작성
+                // 아직 백앤드와 통신하지 않기 때문에 realId와 realPw라는 변수에 임의로 값을 지정해주고 만약 일치할 경우에만 main페이지로 이동하도록 goToMain함수를 실행
+                className='flex h-[50px] w-[350px] items-center
+               justify-center rounded-lg bg-pink-400 text-2xl font-semibold  text-white transition-all
+                hover:bg-pink-500 hover:text-white
+                '
+              >
                 로그인
               </button>
             </div>
