5faa9d89195643987fec64283564d88a9c480d63
Elbyss <elysiabyss@gmail.com>
2024-02-24 23:12:06 +0900

    Design: CSS Styling (2/2)

--- src/app/content/community/page.jsx
+++ src/app/content/community/page.jsx
@@ -2,6 +2,8 @@
 import SideNavbar from '@/components/Community/SideNavbar';
 import { motion } from 'framer-motion';
 import BestLikePost from '@/components/Community/BestPost';
+import { BiSolidPencil } from 'react-icons/bi';
+import PostList from '@/components/Community/PostList';
 export default function CommunityPage() {
   return (
     <>
@@ -13,15 +15,40 @@ export default function CommunityPage() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.2 }}
         >
-          <div className='p-2'>
+          <div className='flex flex-col gap-2 p-2'>
             <h2 className='text-2xl font-bold'>💞 BEST 좋아요</h2>
-            <p className='mb-8 text-sm opacity-50'>가장 많은 좋아요를 받은 게시글이에요.</p>
+            <p className='mb-4 text-sm opacity-50'>가장 많은 좋아요를 받은 게시글이에요.</p>
           </div>
-          <div className='flex w-full justify-center gap-10'>
+          <div className='mb-10 flex w-full justify-center gap-10'>
             <BestLikePost />
             <BestLikePost />
             <BestLikePost />
           </div>
+          {/* 경계선 */}
+          <div className='mb-10 border border-pink-200' />
+          <div className='mb-10 flex gap-4'>
+            <div className='ml-4 text-2xl font-bold'>전체</div>
+            <select className='rounded-lg bg-pink-200 px-2 py-1 font-semibold outline-none'>
+              <option value=''>카테고리 선택</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+            </select>
+            <div className='ml-auto flex w-[100px] items-center justify-center gap-2 rounded-lg bg-pink-400 text-sm font-bold text-white'>
+              <BiSolidPencil color='white' size={18} />글 작성
+            </div>
+          </div>
+          <PostList />
+          <PostList />
+          <PostList />
+          <PostList />
+          <PostList />
+          <PostList />
+          <PostList />
+          <PostList />
+          <PostList />
+          <PostList />
+          <PostList />
         </motion.nav>
       </section>
     </>
--- src/app/content/layout.jsx
+++ src/app/content/layout.jsx
@@ -1,3 +1,4 @@
 export default function layout({ children }) {
-  return <div className='mx-auto flex h-[100dvh] max-w-5xl bg-white'>{children}</div>;
+  // h-[100dvh]
+  return <div className='mx-auto flex max-w-5xl bg-white'>{children}</div>;
 }
