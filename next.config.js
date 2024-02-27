a2fe7e39c1777c2201d9620482289a7ac8b8db8b
Elbyss <elysiabyss@gmail.com>
2024-02-28 00:32:33 +0900

    Chore: next config 수정

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   async rewrites() {
     return [
       {
         source: '/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/:path*`,
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/:path*`,
       },
     ];
   },
