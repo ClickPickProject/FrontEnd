5900df211f5c7d12f8be61500f56e6d94ee0dfc6
Elbyss <elysiabyss@gmail.com>
2024-02-28 16:30:09 +0900

    Refactor: 회원가입 처리 로직 리팩터링 - 중복 로직 제거 및 통합 - 코드 효율성 개선

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -5,19 +5,8 @@ import axios from 'axios';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { MailIcon, NickIcon, PasswordIcon, PhoneIcon, UserNameIcon } from '@/components/UI/Icons';
+import InputWithValidation from '@/components/InputWithValidation';
 export default function SignUpPage() {
-  const [idValid, setIdValid] = useState(false);
-  const [pwValid, setPwValid] = useState(false);
-  const [nameValid, setNameValid] = useState(false);
-  const [nickValid, setNickValid] = useState(false);
-  const [phoneValid, setPhoneValid] = useState(false);
-  const [idDisabled, setIdDisabled] = useState(true);
-  const [nameDisabled, setNameDisabled] = useState(true);
-  const [nickDisabled, setNickDisabled] = useState(true);
-  const [phoneDisabled, setPhoneDisabled] = useState(true);
-  const [idValidCheck, setIdValidCheck] = useState(false);
-  const [nickValidCheck, setNickValidCheck] = useState(false);
-  const [phoneValidCheck, setPhoneValidCheck] = useState(false);
   const [userData, setUserData] = useState({
     id: '',
     pw: '',
@@ -26,111 +15,71 @@ export default function SignUpPage() {
     nickname: '',
     phone: '',
   });
-  const { id, pw, confirmPw, nickname, name, phone } = userData;
-  const idRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
-  const nameRegex = /^[가-힣]{2,5}$/;
-  const nickRegex = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$/;
-  const phoneRegex = /^010(\d{3,4})(\d{4})$/;
-  const router = useRouter();
+  const [inputStatus, setInputStatus] = useState({
+    id: { valid: false, disabled: true },
+    pw: { valid: false },
+    confirmPw: { valid: false },
+    name: { valid: false },
+    nickname: { valid: false, disabled: true },
+    phone: { valid: false, disabled: true },
+  });
 
-  const onChangeId = (e) => {
-    setUserData({ ...userData, id: e.target.value });
-    console.log(e.target.value);
-    idRegex.test(e.target.value) ? setIdValid(true) : setIdValid(false);
-    idValid ? setIdDisabled(false) : setIdDisabled(true);
-  };
-  const onChangePw = (e) => {
-    setUserData({ ...userData, pw: e.target.value });
-    pwRegex.test(e.target.value) ? setPwValid(true) : setPwValid(false);
-  };
-  const onChangeConfirmPw = (e) => {
-    setUserData({ ...userData, confirmPw: e.target.value });
+  // 각 입력 필드의 유효성 검사 정규식
+  const validationRegex = {
+    id: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    pw: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+    name: /^[가-힣]{2,5}$/,
+    nickname: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$/,
+    phone: /^010(\d{3,4})(\d{4})$/,
   };
 
-  const onChangeName = (e) => {
-    setUserData({ ...userData, name: e.target.value });
-    nameRegex.test(e.target.value) ? setNameValid(true) : setNameValid(false);
-    nameValid ? setNameDisabled(false) : setNameDisabled(true);
-  };
-  const onChangeNick = (e) => {
-    setUserData({ ...userData, nickname: e.target.value });
-    nickRegex.test(e.target.value) ? setNickValid(true) : setNickValid(false);
-    nickValid ? setNickDisabled(false) : setNickDisabled(true);
-  };
-
-  const onChangePhone = (e) => {
-    setUserData({ ...userData, phone: e.target.value });
-    phoneRegex.test(e.target.value) ? setPhoneValid(true) : setPhoneValid(false);
-    phoneValid ? setPhoneDisabled(false) : setPhoneDisabled(true);
-  };
-
-  const onClickIdValidCheck = async (e) => {
-    e.preventDefault();
-    if (idValid && !idDisabled) {
-      try {
-        const res = await axios.get(`/api/check/userid/${userData.id}`);
-        if (res.status === 200) {
-          console.log(res);
-          setIdValidCheck(true);
-          router.push('/');
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  };
-  const onClickNickValidCheck = async (e) => {
-    e.preventDefault();
-    if (nickValid && !nickDisabled) {
-      try {
-        const res = await axios.get(`/api/check/nickname/${userData.nickname}`);
-        if (res.status === 200) {
-          console.log(res);
-          setNickValidCheck(true);
-          router.push('/');
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  };
-  const onClickPhoneValidCheck = async (e) => {
-    e.preventDefault();
-    if (phoneValid && !phoneDisabled) {
-      try {
-        const res = await axios.get(`/api/check/phone/${userData.phone}`);
-        if (res.status === 200) {
-          console.log(res);
-          setPhoneValidCheck(true);
-          router.push('/');
-        }
-      } catch (err) {
-        console.log(err);
-      }
+  // 각 입력 필드 상태 관리 함수
+  const handleInputChange = (e, field) => {
+    const { value } = e.target;
+    setUserData({ ...userData, [field]: value });
+    if (field !== 'confirmPw') {
+      // confirmPw는 정규식 검사를 수행하지 않음
+      const isValid = validationRegex[field].test(value);
+      setInputStatus({
+        ...inputStatus,
+        [field]: { valid: isValid, disabled: !isValid },
+      });
     }
   };
 
-  const isSame = pw === confirmPw;
-  const isAllValid = idValid && pwValid && confirmPw && nickValid && nameValid && phoneValid && isSame;
+  // 중복 확인 함수
+  const handleDuplicateCheck = async (field) => {
+    const fieldValue = userData[field];
+    const disabled = inputStatus[field].disabled;
+    if (disabled) return;
+    try {
+      const res = await axios.get(`/api/check/${field}/${fieldValue}`);
+      if (res.status === 200) {
+        setInputStatus({
+          ...inputStatus,
+          [field]: { ...inputStatus[field], valid: true },
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  // 모든 입력 필드가 유효한지 확인하는 함수
+  const isFormValid = () => {
+    return Object.values(inputStatus).every((field) => field.valid);
+  };
+
+  // 회원가입 처리 함수
   const handleRegister = async (e) => {
     e.preventDefault();
-    console.log('handleRegister');
+    if (!isFormValid()) return;
+
     try {
       const res = await axios.post(
         `/api/signup/user`,
-        // `http://localhost:1234/memo`,
-        {
-          id: id,
-          password: pw,
-          name: name,
-          nickname: nickname,
-          phone: phone,
-        },
-        {
-          withCredentials: true,
-        },
+        { id, password: pw, name, nickname, phone },
+        { withCredentials: true },
       );
       if (res.status === 200) {
         alert('회원가입이 완료되었습니다.');
@@ -141,173 +90,133 @@ export default function SignUpPage() {
       alert('회원가입 불가');
     }
   };
-  const onVisible = {
-    id: id.length === 0 ? 'invisible' : 'visible',
-    pw: pw.length === 0 ? 'invisible' : 'visible',
-    confirmPw: confirmPw.length === 0 ? 'invisible' : 'visible',
-    name: name.length === 0 ? 'invisible' : 'visible',
-    nickname: nickname.length === 0 ? 'invisible' : 'visible',
-    phone: phone.length === 0 ? 'invisible' : 'visible',
-  };
 
-  const defaultInputStyle = 'rounded-lg outline-none h-[45px] pl-7';
-  const errorMsgStyle = 'flex w-[350px] font-semibold text-red-600';
+  const { id, pw, confirmPw, nickname, name, phone } = userData;
+  const router = useRouter();
+  const userDataFields = {
+    id,
+    pw,
+    confirmPw,
+    name,
+    nickname,
+    phone,
+  };
+  const onVisible = {};
+  for (const key in userDataFields) {
+    onVisible[key] = userDataFields[key].length === 0 ? 'invisible' : 'visible';
+  }
 
   return (
     <>
       <div className='h-[100dvh] bg-[#fdf4f5]'>
-        <section className='absolute left-1/2 top-1/2 flex h-[900px] w-[500px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-2xl bg-pink-200  shadow-[1px_1px_200px_1px]  shadow-pink-200'>
+        <section className='absolute left-1/2 top-1/2 flex h-[780px] w-[500px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-2xl bg-pink-200  shadow-[1px_1px_200px_1px]  shadow-pink-200'>
           <figure className='mx-auto p-6'>
             <Link href='/'>
               <Image src='/Images/clickpick_icon.png' alt='' width={52} height={52} />
             </Link>
           </figure>
           <h2 className='mx-auto mb-8 text-2xl font-bold'>클릭픽 회원가입</h2>
-          <form onSubmit={handleRegister} className='j flex w-full flex-col items-center justify-center gap-4'>
-            <div className='relative flex w-[350px] flex-col'>
-              <MailIcon size={20} />
-              <input
-                placeholder='이메일'
-                type='email'
-                id='id'
-                // value={userData.id}
-                onChange={onChangeId}
-                required
-                className={`${defaultInputStyle}`}
-              />
-              <div className='absolute right-3 flex h-full items-center'>
-                <button
-                  disabled={idDisabled}
-                  onClick={onClickIdValidCheck}
-                  className={`${idValid === true ? 'bg-pink-300 hover:bg-pink-500 hover:text-white' : 'bg-gray-200'}
-                  rounded-md px-3 py-1 text-sm transition-all `}
-                >
-                  중복 확인
-                </button>
-              </div>
-            </div>
+          <form onSubmit={handleRegister} className='flex w-full flex-col items-center justify-center gap-2'>
+            {/* 이메일 입력 필드 */}
+            <InputWithValidation
+              label='이메일'
+              icon={<MailIcon size={20} />}
+              type='email'
+              id='id'
+              value={id}
+              onChange={(e) => handleInputChange(e, 'id')}
+              onClickValidCheck={() => handleDuplicateCheck('id')}
+              valid={inputStatus.id.valid}
+              disabled={inputStatus.id.disabled}
+              errorMsg='이메일 주소가 올바르지 않습니다.'
+              correctMsg='올바른 이메일 주소입니다.'
+              onVisible={onVisible.id}
+            />
 
-            {idValid ? (
-              <div className={`flex w-[350px] font-semibold text-blue-600`}>
-                {idValidCheck ? '사용 가능한 이메일입니다' : '올바른 이메일 주소입니다.'}
-              </div>
-            ) : (
-              <div className={`${onVisible.id} ${errorMsgStyle}`}>이메일 주소가 올바르지 않습니다.</div>
-            )}
-            <div className='relative flex w-[350px] flex-col'>
-              <PasswordIcon size={20} />
-              <input
-                placeholder='비밀번호'
-                type='password'
-                id='pw'
-                onChange={onChangePw}
-                required
-                className={`${defaultInputStyle}`}
-                value={pw}
-              />
-            </div>
-            {pwValid ? (
-              <div className='flex w-[350px] font-semibold text-blue-600'>사용할 수 있는 비밀번호입니다.</div>
-            ) : (
-              <div className={`${onVisible.pw} ${errorMsgStyle}`}>영문과 숫자를 포함하여 6자 이상 입력해주세요</div>
-            )}
+            {/* 비밀번호 입력 필드 */}
+            <InputWithValidation
+              label='비밀번호'
+              icon={<PasswordIcon size={20} />}
+              type='password'
+              id='pw'
+              value={pw}
+              onChange={(e) => handleInputChange(e, 'pw')}
+              valid={inputStatus.pw.valid}
+              errorMsg='영문과 숫자를 포함하여 6자 이상 입력해주세요'
+              correctMsg='사용할 수 있는 비밀번호입니다.'
+              onVisible={onVisible.pw}
+            />
 
-            <div className='relative flex w-[350px] flex-col'>
-              <PasswordIcon size={20} />
-              <input
-                placeholder='비밀번호 확인'
-                type='password'
-                id='confirmPw'
-                onChange={onChangeConfirmPw}
-                required
-                className={`${defaultInputStyle}`}
-              />
-            </div>
-            {isSame ? (
-              <div className={`${onVisible.confirmPw} w-[350px] font-semibold text-blue-600`}>
-                비밀번호가 서로 일치합니다.
-              </div>
-            ) : (
-              <div className={`${onVisible.confirmPw} ${errorMsgStyle}`}>비밀번호가 서로 일치하지 않습니다.</div>
-            )}
+            {/* 비밀번호 확인 입력 필드 */}
+            <InputWithValidation
+              label='비밀번호 확인'
+              icon={<PasswordIcon size={20} />}
+              type='password'
+              id='confirmPw'
+              value={confirmPw}
+              onChange={(e) => handleInputChange(e, 'confirmPw')}
+              valid={pw === confirmPw}
+              errorMsg='비밀번호가 서로 일치하지 않습니다.'
+              correctMsg='비밀번호가 서로 일치합니다.'
+              onVisible={onVisible.confirmPw}
+            />
 
-            <div className='relative flex w-[350px] flex-col'>
-              <UserNameIcon size={20} />
-              <input
-                placeholder='이름'
-                type='text'
-                id='name'
-                onChange={onChangeName}
-                required
-                className={`${defaultInputStyle}`}
-              />
-            </div>
-            {nameValid ? (
-              <div className='flex w-[350px] font-semibold text-blue-500'>올바른 이름입니다.</div>
-            ) : (
-              <div className={`${onVisible.name} ${errorMsgStyle}`}>이름이 올바르지 않습니다.</div>
-            )}
-            <div className='relative flex w-[350px] flex-col'>
-              <NickIcon size={20} />
-              <input
-                placeholder='닉네임'
-                type='text'
-                id='nickname'
-                onChange={onChangeNick}
-                required
-                className={`${defaultInputStyle}`}
-              />
-              <div className='absolute right-3 flex h-full items-center'>
-                <button
-                  onClick={onClickNickValidCheck}
-                  disabled={nameDisabled}
-                  className={`${nickValid ? 'bg-pink-300' : 'bg-gray-200'} rounded-md px-3 py-1 text-sm transition-all hover:bg-pink-500 hover:text-white`}
-                >
-                  중복 확인
-                </button>
-              </div>
-            </div>
-            {nickValid ? (
-              <div className='flex w-[350px] font-semibold text-blue-500'>
-                {nickValidCheck ? '사용 가능한 닉네임입니다.' : '올바른 닉네임입니다.'}
-              </div>
-            ) : (
-              <div className={`${onVisible.nickname} ${errorMsgStyle}`}>
-                2~16자 사이의 영어, 숫자, 한글로 입력해주세요
-              </div>
-            )}
-            <div className='relative flex w-[350px] flex-col'>
-              <PhoneIcon size={24} />
-              <input
-                placeholder='휴대폰 번호'
-                type='text'
-                id='phone'
-                onChange={onChangePhone}
-                required
-                className={`${defaultInputStyle}`}
-              />
-              <div className='absolute right-3 flex h-full items-center'>
-                <button
-                  onClick={onClickPhoneValidCheck}
-                  disabled={phoneDisabled}
-                  className={`${phoneValid ? 'bg-pink-300' : 'bg-gray-200'} rounded-md px-3 py-1 text-sm transition-all hover:bg-pink-500 hover:text-white`}
-                >
-                  중복 확인
-                </button>
-              </div>
-            </div>
-            {phoneValid ? (
-              <div className='flex w-[350px] font-semibold text-blue-500'>
-                {phoneValidCheck ? '사용 가능한 번호입니다.' : '올바른 번호입니다.'}
-              </div>
-            ) : (
-              <div className={`${onVisible.phone} ${errorMsgStyle}`}>하이픈을 제외한 숫자를 입력해주세요.</div>
-            )}
+            {/* 이름 입력 필드 */}
+            <InputWithValidation
+              label='이름'
+              icon={<UserNameIcon size={20} />}
+              type='text'
+              id='name'
+              value={name}
+              onChange={(e) => handleInputChange(e, 'name')}
+              onClickValidCheck={() => handleDuplicateCheck('name')}
+              valid={inputStatus.name.valid}
+              disabled={inputStatus.name.disabled}
+              errorMsg='이름이 올바르지 않습니다.'
+              correctMsg='올바른 이름입니다.'
+              onVisible={onVisible.name}
+            />
+
+            {/* 닉네임 입력 필드 */}
+            <InputWithValidation
+              label='닉네임'
+              icon={<NickIcon size={20} />}
+              type='text'
+              id='nickname'
+              value={nickname}
+              onChange={(e) => handleInputChange(e, 'nickname')}
+              onClickValidCheck={() => handleDuplicateCheck('nickname')}
+              valid={inputStatus.nickname.valid}
+              disabled={inputStatus.nickname.disabled}
+              errorMsg='2~16자 사이의 영어, 숫자, 한글로 입력해주세요'
+              correctMsg='사용 가능한 닉네임입니다.'
+              onVisible={onVisible.nickname}
+            />
+
+            {/* 휴대폰 번호 입력 필드 */}
+            <InputWithValidation
+              label='휴대폰 번호'
+              icon={<PhoneIcon size={24} />}
+              type='text'
+              id='phone'
+              value={phone}
+              onChange={(e) => handleInputChange(e, 'phone')}
+              onClickValidCheck={() => handleDuplicateCheck('phone')}
+              valid={inputStatus.phone.valid}
+              disabled={inputStatus.phone.disabled}
+              errorMsg='하이픈을 제외한 숫자를 입력해주세요.'
+              correctMsg='사용 가능한 번호입니다.'
+              onVisible={onVisible.phone}
+            />
+
+            {/* 회원가입 버튼 */}
             <div className='flex justify-center'>
               <button
                 type='submit'
-                className={`flex h-[45px] w-[350px]  items-center justify-center rounded-lg bg-pink-400 text-xl font-semibold text-white transition-all hover:text-white ${isAllValid ? 'opacity-100' : 'opacity-50'}`}
-                disabled={!isAllValid}
+                className={`flex h-[45px] w-[350px]  items-center justify-center rounded-lg bg-pink-400 text-xl font-semibold text-white transition-all hover:text-white ${
+                  isFormValid() ? 'opacity-100' : 'opacity-50'
+                }`}
+                disabled={!isFormValid()}
               >
                 가입하기
               </button>
---
+++ src/components/InputWithValidation.jsx
@@ -0,0 +1,54 @@
+'use client';
+export default function InputWithValidation({
+  label,
+  icon,
+  type,
+  id,
+  value,
+  onChange,
+  onClickValidCheck,
+  valid,
+  disabled,
+  errorMsg,
+  correctMsg,
+  onVisible,
+}) {
+  const defaultInputStyle = 'h-[45px] rounded-lg pl-7 outline-none';
+  const correctMsgStyle = 'flex w-[350px] text-sm text-blue-600';
+  const errorMsgStyle = 'flex w-[350px] text-sm text-red-600';
+  const validCheckVisible =
+    label === '비밀번호' || label === '비밀번호 확인' || label === '이름' ? 'invisible' : 'visible';
+  return (
+    <>
+      <div className='relative flex w-[350px] flex-col'>
+        {icon}
+        <input
+          placeholder={label}
+          type={type}
+          id={id}
+          value={value}
+          onChange={onChange}
+          required
+          className={`${defaultInputStyle}`}
+        />
+        <div className={`absolute right-2 flex h-full items-center ${validCheckVisible}`}>
+          <button
+            disabled={disabled}
+            onClick={(e) => {
+              e.preventDefault();
+              onClickValidCheck();
+            }}
+            className={`${valid ? 'bg-pink-300 hover:bg-pink-500 hover:text-white' : 'bg-gray-200'} rounded-md px-3 py-1 text-sm transition-all`}
+          >
+            중복 확인
+          </button>
+        </div>
+      </div>
+      {valid ? (
+        <div className={`${onVisible} ${correctMsgStyle}`}>{correctMsg}</div>
+      ) : (
+        <div className={`${onVisible} ${errorMsgStyle}`}>{errorMsg}</div>
+      )}
+    </>
+  );
+}
