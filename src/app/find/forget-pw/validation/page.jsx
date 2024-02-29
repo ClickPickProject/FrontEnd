9776e5ea81082b9c56ba3200e2d1d349725b0b9e
Elbyss <elysiabyss@gmail.com>
2024-02-29 19:06:21 +0900

    Feat: 비밀번호 찾기 및 인증 페이지 추가

---
+++ src/app/find/forget-pw/validation/page.jsx
@@ -0,0 +1,116 @@
+'use client';
+import { MailIcon, PhoneIcon, UserNameIcon } from '@/components/UI/Icons';
+import axios from 'axios';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function ValidationPage() {
+  const defaultInputStyle = 'rounded-lg outline-none h-[45px] pl-7';
+  const [viewName, setViewName] = useState('');
+  const [foundId, setFoundId] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [findIdInfo, setFindIdInfo] = useState({
+    name: '',
+    phone: '',
+  });
+
+  const validationRegex = {
+    name: /^[가-힣]{2,5}$/,
+    phone: /^010(\d{3,4})(\d{4})$/,
+  };
+
+  const validationRegexCheck =
+    !validationRegex.name.test(findIdInfo.name) || !validationRegex.phone.test(findIdInfo.phone);
+
+  const onChangefindIdInfo = (e, field) => {
+    const { value } = e.target;
+    setFindIdInfo({ ...findIdInfo, [field]: value });
+  };
+
+  const handleIdFindSubmit = async (e) => {
+    e.preventDefault();
+    const { name, phone } = findIdInfo;
+    if (validationRegexCheck) {
+      setIsValid(false);
+      return;
+    }
+    try {
+      const res = await axios.post('http://localhost:3001/member', { name, phone });
+      setFindIdInfo({ name: '', phone: '' });
+      setViewName(name);
+      setFoundId(res.data.id);
+      setIsValid(true);
+      router.push('./validation');
+      if (res.status === 200) {
+        setViewName(findIdInfo.name);
+        setFoundId(res.data.id);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <div className='h-[100dvh] bg-[#fdf4f5]'>
+        <section className='absolute left-1/2 top-1/2 flex h-[550px] w-[500px] -translate-x-1/2 -translate-y-1/2 flex-col  rounded-2xl  bg-pink-200  shadow-[1px_1px_200px_1px] shadow-pink-200'>
+          <figure className='mx-auto p-6'>
+            <Link href='/'>
+              <Image src='/Images/clickpick_icon.png' alt='' width={52} height={52} />
+            </Link>
+          </figure>
+          <h2 className='mx-auto mb-8 flex flex-col items-center justify-center text-xl font-bold'>
+            인증번호를 입력해주세요
+            <p className='text-sm font-normal'>해당 이메일로 인증번호를 발송하였습니다.</p>
+          </h2>
+          <form onSubmit={handleIdFindSubmit} className='flex w-full flex-col items-center justify-center gap-8'>
+            <div className='relative flex w-[350px] flex-col'>
+              <MailIcon size={20} />
+              <input
+                placeholder='인증번호'
+                type='text'
+                id='id'
+                value={findIdInfo.name}
+                onChange={(e) => onChangefindIdInfo(e, 'name')}
+                required
+                className={`${defaultInputStyle}`}
+              />
+            </div>
+            <div className='flex justify-center'>
+              <button
+                type='submit'
+                disabled={validationRegexCheck}
+                className={`${validationRegexCheck ? 'opacity-50' : 'opacity-100 hover:bg-pink-500 hover:text-white'} h-[45px] w-[350px] items-center justify-center rounded-lg bg-pink-400 text-xl font-semibold text-white transition-all`}
+              >
+                확인
+              </button>
+            </div>
+            <div className='flex text-gray-600 '>
+              인증번호을 받지 못하셨나요?
+              <div className='pl-2 font-semibold text-black'>
+                <Link href='/find/forget-pw' className=''>
+                  재전송
+                </Link>
+              </div>
+            </div>
+            <div className='flex text-gray-600 '>
+              <div className={`pl-2 font-semibold text-black`}>
+                {isValid ? (
+                  <p>
+                    <span className='text-pink-500'>{viewName}</span>님의 아이디는
+                    <span className='text-pink-500'> {foundId} </span>입니다.
+                  </p>
+                ) : (
+                  <p className={`text-sm text-red-500 ${isValid ? 'visible' : 'invisible'}`}>
+                    이름 또는 번호를 찾을 수 없습니다.
+                  </p>
+                )}
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+    </>
+  );
+}
