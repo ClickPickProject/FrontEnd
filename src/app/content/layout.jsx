1baeba18a02fe649da22654ed8f9b2d0fe845394
Elbyss <elysiabyss@gmail.com>
2024-03-01 18:06:58 +0900

    Design: 레이아웃 최대 너비 조절

--- src/app/content/layout.jsx
+++ src/app/content/layout.jsx
@@ -1,4 +1,4 @@
 export default function layout({ children }) {
   // h-[100dvh]
-  return <div className='mx-auto flex max-w-5xl bg-white'>{children}</div>;
+  return <div className='mx-auto flex max-w-7xl bg-white'>{children}</div>;
 }
