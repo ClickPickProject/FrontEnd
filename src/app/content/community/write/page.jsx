4d39eac2628df6b083027d0e7a58eef7a10a8a61
Elbyss <elysiabyss@gmail.com>
2024-03-05 23:29:44 +0900

    Feat: 게시글 작성 페이지 API 통신 로직 추가 및 빈 텍스트 필터링

--- src/app/content/community/write/page.jsx
+++ src/app/content/community/write/page.jsx
@@ -1,41 +1,69 @@
 'use client';
+import { editorContentState, editorTagState, editorTitleState } from '@/atoms/editorContentState';
 import Hashtag from '@/components/Community/Hashtag';
-import SideNavbar from '@/components/Community/SideNavbar';
+import CustomEditor from '@/components/CustomEditor';
 import DropDownMenu from '@/components/UI/DropDownMenu';
-import dynamic from 'next/dynamic';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 export default function WritePage() {
-  const CustomEditor = dynamic(
-    () => {
-      return import('@/components/CustomEditor');
-    },
-    { ssr: false, loading: () => '로딩중입니다...' },
-  );
-  const handleWriteSubmit = (e) => {
+  const [title, setTitle] = useRecoilState(editorTitleState);
+  const content = useRecoilValue(editorContentState);
+  const tag = useRecoilValue(editorTagState);
+  const router = useRouter();
+  useEffect(() => {
+    setTitle('');
+  }, [setTitle]);
+  const onClickWriteSubmit = async (e) => {
     e.preventDefault();
-    console.log(e.target.value);
+    if (title.length === 0 || content.length === 0) {
+      alert('제목 또는 내용이 존재하지 않습니다.');
+      return;
+    }
+    const body = {
+      userId: 'testAccount',
+      title,
+      content,
+      position: '',
+      hashtag: tag,
+    };
+    try {
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_LOCAL_API_URL}/posts`, body);
+      if (res.status === 200 || 201) {
+        alert('게시글이 등록되었습니다.');
+        router.back();
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <>
-      <SideNavbar />
       <div>
         <div className='p-4 text-2xl font-bold'>글 작성</div>
-        <form onSubmit={handleWriteSubmit} className='flex flex-col'>
+        <div className='flex flex-col'>
           <DropDownMenu />
-          <input placeholder='제목을 입력해주세요' className='h-12 pl-2 text-2xl outline-none' />
+          <input
+            placeholder='제목을 입력해주세요'
+            className='h-12 pl-2 text-2xl outline-none'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
           {/* 에디터 */}
           <div className='h-[full] w-[full]'>
-            <CustomEditor initialData='' />
+            <CustomEditor />
           </div>
           {/* 해시태그 */}
           <Hashtag />
           <div className='mx-auto mt-4 flex h-10 w-1/6 cursor-pointer items-center justify-center rounded-lg bg-pink-300 font-semibold shadow-md transition-all hover:bg-pink-400 '>
-            <button type='submit' className=''>
+            <button onClick={onClickWriteSubmit} className='h-full w-full'>
               제출
             </button>
           </div>
-        </form>
+        </div>
       </div>
     </>
   );
