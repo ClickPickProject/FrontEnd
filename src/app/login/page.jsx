5f47c0777d5ff21159736e9874dea38ac4f35535
Jungmu <wjdan022@naver.com>
2024-03-10 00:17:56 +0900

    Login  구현부

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,22 +1,43 @@
 'use client';
+import { MdOutlineMailOutline } from 'react-icons/md';
+import { RiLockPasswordLine } from 'react-icons/ri';
 import Image from 'next/image';
 import Link from 'next/link';
-import { MailIcon, PasswordIcon } from '@/components/UI/Icons';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import axios from 'axios';
+
 export default function LoginPage() {
   let [id, setId] = useState(''); // setState로 id 초기값 공백
   let [pw, setPw] = useState(''); // setState로 password초기값 공백
   const [button, setButton] = useState(true);
+  const router = useRouter();
+  const goToMain = () => {
+    router.push('/');
+    // router.push('/');최상단 root로 가는길
+  };
+  const defaultInputStyle = 'rounded-lg outline-none h-[45px] pl-7';
   function changeButton() {
     id.includes('@') && pw.length >= 5 ? setButton(false) : setButton(true);
-  }
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  } // changeButton()함수는 id에 @가 포함되어있고 pw의 글자가 5글자 이상일때 조건식이 맞을때 false, 맞지 않을때 true로 견경해줌.
+  //API요청하는코드
 
+  const isFormValid = () => {
+    const allowedProperties = ['id', 'nickname', 'phone'];
+    return (
+      Object.values(inputStatus).every((field) => field.valid) &&
+      Object.keys(inputStatus)
+        .filter((key) => allowedProperties.includes(key))
+        .every((key) => inputStatus[key].duplicateCheck)
+    );
+  };
+
+  // post방식으로 axios 사용하기.
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    if (0) return;
     try {
-      const res = await axios.post(`http://localhost:3001/api/login`, { id, pw }, { withCredentials: true });
+      const res = await axios.post(`http://localhost:3001/login`, { userId, userPw }, { withCredentials: true });
       if (res.status === 200) {
         alert('로그인이 완료되었습니다.');
         router.push('/');
@@ -27,7 +48,6 @@ export default function LoginPage() {
     }
   };
 
-  const defaultInputStyle = 'rounded-lg outline-none h-[45px] pl-7';
   return (
     <>
       <div className='h-[100dvh] bg-[#fdf4f5]'>
@@ -36,9 +56,12 @@ export default function LoginPage() {
             <Image src='/Images/clickpick_icon.png' alt='' width={52} height={52} />
           </figure>
           <h2 className='mx-auto mb-8 text-2xl font-bold'>클릭픽 로그인</h2>
-          <form className='flex w-full flex-col items-center justify-center gap-8'>
+          <form onSubmit={handleRegister} className='flex w-full flex-col items-center justify-center gap-8'>
             <div className='relative flex w-[350px] flex-col'>
-              <MailIcon size={20} />
+              <MdOutlineMailOutline
+                size={20}
+                className='absolute ml-1 flex h-full items-center justify-center opacity-50'
+              />
               <input
                 placeholder='아이디'
                 type='text'
@@ -50,9 +73,13 @@ export default function LoginPage() {
                 }}
                 onKeyUp={changeButton}
               />
+              {/* //input에 입력되는 내용이 바뀔때마다 e.target.value의 값이 id로 담김 */}
             </div>
             <div className='relative flex w-[350px] flex-col'>
-              <PasswordIcon size={20} />
+              <RiLockPasswordLine
+                size={20}
+                className='absolute ml-1 flex h-full items-center justify-center opacity-50'
+              />
               <input
                 placeholder='비밀번호'
                 type='password'
@@ -64,9 +91,23 @@ export default function LoginPage() {
                 }}
                 onKeyUp={changeButton}
               />
+              {/* input에 입력되는 내용이 바뀔때마다 e.target.value의 값이 pw로 담김 */}
             </div>
+
             <div className='flex justify-center'>
-              <button className='flex h-[50px] w-[350px]  items-center justify-center rounded-lg bg-pink-400 text-2xl font-semibold text-white transition-all hover:bg-pink-500 hover:text-white'>
+              <button
+                type='button'
+                disabled={button}
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                // button의 값이 true일경우 disabled가 작동 false일 경우 disabled가 작동하지 않도록 disabled={button}을 작성
+                // 아직 백앤드와 통신하지 않기 때문에 realId와 realPw라는 변수에 임의로 값을 지정해주고 만약 일치할 경우에만 main페이지로 이동하도록 goToMain함수를 실행
+                className='flex h-[50px] w-[350px] items-center
+               justify-center rounded-lg bg-pink-400 text-2xl font-semibold  text-white transition-all
+                hover:bg-pink-500 hover:text-white
+                '
+              >
                 로그인
               </button>
             </div>
