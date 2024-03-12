dfaf09d56f6734b2c1fcee195b64543eb7818d65
Elbyss <elysiabyss@gmail.com>
2024-03-13 01:18:17 +0900

    Feat: 좋아요 기능 개선

--- src/components/Community/PostDetail.jsx
+++ src/components/Community/PostDetail.jsx
@@ -5,34 +5,67 @@ import { IoMdHeart, IoMdHeartEmpty } from 'react-icons/io';
 import { FaRegCommentDots } from 'react-icons/fa6';
 import { useParams } from 'next/navigation';
 import HashtagView from './HashtagView';
-import { useRecoilValue } from 'recoil';
-import { postSelectorFamily } from '@/atoms/PostState';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Comment from './Comment';
 export default function PostDetail() {
   const params = useParams();
-  const userPost = useRecoilValue(postSelectorFamily(params.id));
+  const [userPost, setUserPost] = useState({});
   const [likeCount, setLikeCount] = useState(userPost.likeCount);
-  const [isLiked, setIsLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState(userPost.likePostCheck);
 
+  useEffect(() => {
+    const postUpdate = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const res = await axios.get(`/api/post/${params.id}`, {
+          withCredentials: true,
+          headers: {
+            Authorization: token,
+          },
+        });
+        if (res.status === 200) {
+          setUserPost(res.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    postUpdate();
+  }, []);
+
+  useEffect(() => {
+    setIsLiked(userPost.likePostCheck);
+    setLikeCount(userPost.likeCount);
+  }, [userPost.likePostCheck, userPost.likeCount]);
   const onClickLike = async () => {
     try {
-      if (isLiked) {
-        // unlike
-        await axios.get(`/api/member/likedpost/${params.id}`);
-        setLikeCount(likeCount - 1);
-      } else {
-        // like
-        await axios.get(`/api/member/likedpost/${params.id}`);
+      const token = localStorage.getItem('token');
+
+      if (isLiked === true) {
+        await axios.get(`/api/member/likedpost/${params.id}`, {
+          withCredentials: true,
+          headers: {
+            Authorization: token,
+          },
+        });
+        setIsLiked(false);
+        setLikeCount(likeCount > 0 ? likeCount - 1 : 0);
+      }
+      if (isLiked === false) {
+        await axios.get(`/api/member/likedpost/${params.id}`, {
+          withCredentials: true,
+          headers: {
+            Authorization: token,
+          },
+        });
+        setIsLiked(true);
         setLikeCount(likeCount + 1);
       }
-      setIsLiked(!isLiked);
     } catch (err) {
       console.error('좋아요 오류', err);
     }
   };
-
   return (
     <>
       <div className='w-full max-w-[830px]'>
