f946efdd192cc46316a014180f6ca10c6b2d1cc2
Elbyss <elysiabyss@gmail.com>
2024-03-13 22:47:08 +0900

    Feat: 댓글 추가 및 스타일링

--- src/components/Community/Comment.jsx
+++ src/components/Community/Comment.jsx
@@ -1,15 +1,24 @@
+import { MdReportGmailerrorred } from 'react-icons/md';
 import WriterView from './BestPost/WriterView';
 
-export default function Comment() {
+export default function Comment({ comments }) {
+  if (!comments || comments.length === 0) {
+    return null;
+  }
   return (
     <div className=''>
       <ul>
-        <li className='flex flex-col gap-1'>
-          <WriterView />
-          <div>그렇군요...</div>
-          <div className='text-sm font-semibold opacity-50'>답글</div>
-          <div className='border' />
-        </li>
+        {comments.map((comment) => (
+          <li className='flex flex-col gap-3'>
+            <WriterView writer={comment.nickname} date={comment.createAt} />
+            <div>{comment.content}</div>
+            <div className='flex items-center gap-1'>
+              <MdReportGmailerrorred color='red' opacity='70%' />
+              <div className='text-sm font-semibold opacity-50'>신고</div>
+            </div>
+            <div className='my-4 border' />
+          </li>
+        ))}
       </ul>
     </div>
   );
