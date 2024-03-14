5ba2b4d8c56ed7fde2c99c6924dafcd29348000e
Elbyss <elysiabyss@gmail.com>
2024-03-15 00:32:09 +0900

    Feat: rewrite 반환값 추가

--- next.config.js
+++ next.config.js
@@ -8,6 +8,21 @@ const nextConfig = {
         source: '/api/login',
         destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/login`,
       },
+      // 회원가입
+      {
+        source: '/api/signup/user',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/signup/user`,
+      },
+      // 중복확인
+      {
+        source: '/api/check/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/check/:path*`,
+      },
+      // 중복확인
+      {
+        source: '/api/verification',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/verification`,
+      },
 
       // 게시글 목록, 베스트 게시글
       {
@@ -30,6 +45,11 @@ const nextConfig = {
       {
         source: '/api/member/likedpost/:path*',
         destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/likedpost/:path*`,
+      },
+      // 댓글 작성/수정/삭제
+      {
+        source: '/api/member/comment/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/comment/:path*`,
       },
     ];
   },
