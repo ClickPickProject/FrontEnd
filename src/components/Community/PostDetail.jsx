53243f3bd5504b970a7d79b9a0a5e31aa32c7069
Elbyss <elysiabyss@gmail.com>
2024-03-15 00:36:57 +0900

    Feat: 포스트 로딩메세지 추가 및 일부 로직 수정

--- src/components/Community/PostDetail.jsx
+++ src/components/Community/PostDetail.jsx
@@ -7,17 +7,25 @@ import { useParams } from 'next/navigation';
 import HashtagView from './HashtagView';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Comment from './Comment';
+import Comment from './Comments';
+import { tokenState } from '@/atoms/tokenState';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { userPostState } from '@/atoms/editorContentState';
+import CommentWrite from './CommentWrite';
+import { commentsState } from '@/atoms/PostState';
+import Comments from './Comments';
 export default function PostDetail() {
   const params = useParams();
-  const [userPost, setUserPost] = useState({});
+  const [userPost, setUserPost] = useRecoilState(userPostState);
   const [likeCount, setLikeCount] = useState(userPost.likeCount);
   const [isLiked, setIsLiked] = useState(userPost.likePostCheck);
+  const token = useRecoilValue(tokenState);
+  const [comments, setComments] = useRecoilState(commentsState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const postUpdate = async () => {
       try {
-        const token = localStorage.getItem('token');
         const res = await axios.get(`/api/post/${params.id}`, {
           withCredentials: true,
           headers: {
@@ -26,6 +34,8 @@ export default function PostDetail() {
         });
         if (res.status === 200) {
           setUserPost(res.data);
+          setComments(res.data.comments);
+          setLoading(false);
         }
       } catch (err) {
         console.log(err);
@@ -40,8 +50,6 @@ export default function PostDetail() {
   }, [userPost.likePostCheck, userPost.likeCount]);
   const onClickLike = async () => {
     try {
-      const token = localStorage.getItem('token');
-
       if (isLiked === true) {
         await axios.get(`/api/member/likedpost/${params.id}`, {
           withCredentials: true,
@@ -68,37 +76,42 @@ export default function PostDetail() {
   };
   return (
     <>
-      <div className='w-full max-w-[830px]'>
-        <div className='my-8 flex flex-col gap-4'>
-          <h2 className='text-2xl font-semibold'>{userPost.title}</h2>
-          {/* 작성자 */}
-          <div className='flex justify-between'>
-            <WriterView writer={userPost.nickname} date={userPost.date} />
-            <StatusView viewCount={userPost.viewCount} likeCount={userPost.likeCount} />
+      {loading ? (
+        '로딩중...'
+      ) : (
+        <div className='w-full max-w-[830px]'>
+          <div className='my-8 flex flex-col gap-4'>
+            <h2 className='text-2xl font-semibold'>{userPost.title}</h2>
+            {/* 작성자 */}
+            <div className='flex justify-between'>
+              <WriterView writer={userPost.nickname} date={userPost.date} />
+              <StatusView viewCount={userPost.viewCount} likeCount={userPost.likeCount} />
+            </div>
           </div>
+          {/* 내용 */}
+          <div className='p-4'>
+            <div dangerouslySetInnerHTML={{ __html: userPost.content }} />
+          </div>
+          {/* 해쉬태그 */}
+          <div className='mb-4'>
+            <HashtagView tags={userPost.hashtags} />
+          </div>
+          <div className='flex items-center gap-1 text-base '>
+            {isLiked ? (
+              <IoMdHeart size={20} color='red' onClick={onClickLike} className='hover:cursor-pointer' />
+            ) : (
+              <IoMdHeartEmpty size={20} color='red' onClick={onClickLike} className='hover:cursor-pointer' />
+            )}
+            좋아요 {likeCount}
+            <FaRegCommentDots size={18} />
+            댓글 {userPost.commentCount}
+          </div>
+          {/* 경계선 */}
+          <div className='my-4 border-b-2' />
+          <Comments />
+          <CommentWrite />
         </div>
-        {/* 내용 */}
-        <div className='p-4'>
-          <div dangerouslySetInnerHTML={{ __html: userPost.content }} />
-        </div>
-        {/* 해쉬태그 */}
-        <div className='mb-4'>
-          <HashtagView tags={userPost.hashtags} />
-        </div>
-        <div className='flex items-center gap-1'>
-          {isLiked ? (
-            <IoMdHeart size={20} color='red' onClick={onClickLike} className='hover:cursor-pointer' />
-          ) : (
-            <IoMdHeartEmpty size={20} color='red' onClick={onClickLike} className='hover:cursor-pointer' />
-          )}
-          좋아요 {likeCount}
-          <FaRegCommentDots size={18} />
-          댓글 {userPost.CommentCount}
-        </div>
-        {/* 경계선 */}
-        <Comment />
-        <div className='mb-8 mt-4 border-b-2' />
-      </div>
+      )}
     </>
   );
 }
