77357649b84840eadd9693a19daab775da887a04
Elbyss <elysiabyss@gmail.com>
2024-03-16 23:10:05 +0900

    Design: 로딩바 스타일링

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 import { BeatLoader } from 'react-spinners';
 
-export default function Loading({ isLoading, isError }) {
-  if (isLoading) return <BeatLoader />;
+export default function Loading({ isPending, isError }) {
+  if (isPending) return <BeatLoader color='hotpink' className='flex w-full items-center justify-center' />;
   if (isError) return <div>불러오는 중 에러가 발생하였습니다.</div>;
 }
