84c11294acd777ed8196f49b1e6e339a5ff3f57d
Elbyss <elysiabyss@gmail.com>
2024-03-16 14:50:27 +0900

    Feat: Loading Logo 추가

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,4 +1,6 @@
+import { BeatLoader } from 'react-spinners';
+
 export default function Loading({ isLoading, isError }) {
-  if (isLoading) return <div>로딩중입니다...</div>;
+  if (isLoading) return <BeatLoader />;
   if (isError) return <div>불러오는 중 에러가 발생하였습니다.</div>;
 }
