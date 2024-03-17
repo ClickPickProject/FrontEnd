05ddef7a28703a5552de06efab48007ac8d78785
Elbyss <elysiabyss@gmail.com>
2024-03-17 20:04:23 +0900

    Design: Loading 표시 위치 수정

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,11 @@
 import { BeatLoader } from 'react-spinners';
 
 export default function Loading({ isPending, isError }) {
-  if (isPending) return <BeatLoader color='hotpink' className='flex w-full items-center justify-center' />;
+  if (isPending)
+    return (
+      <div className='flex h-full w-full items-center justify-center'>
+        <BeatLoader color='hotpink' />
+      </div>
+    );
   if (isError) return <div>불러오는 중 에러가 발생하였습니다.</div>;
 }
