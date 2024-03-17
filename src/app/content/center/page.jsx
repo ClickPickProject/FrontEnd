d9d17e4c9719a3d60ae6409ca99be81bddad0c80
Elbyss <elysiabyss@gmail.com>
2024-03-17 15:21:07 +0900

    Feat: 페이지 추가 - 공지사항, 소통센터

--- src/app/content/center/page.jsx
+++ src/app/content/center/page.jsx
@@ -1,3 +1,25 @@
+'use client';
+import NoticePostList from '@/components/NoticePostList';
+import { motion } from 'framer-motion';
 export default function CenterPage() {
-  return <>소통센터</>;
+  return (
+    <>
+      <section className='flex w-[inherit] flex-col justify-center'>
+        <motion.nav
+          className='menu'
+          initial={{ opacity: 0, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <div className='flex flex-col gap-2 p-2'>
+            <h2 className='text-2xl font-bold'>⭐ 소통센터</h2>
+            <p className='mb-4 text-sm opacity-50'>개선사항을 함께 얘기해보세요.</p>
+          </div>
+          {/* 경계선 */}
+          <div className='mb-10 border border-pink-200' />
+          <NoticePostList />
+        </motion.nav>
+      </section>
+    </>
+  );
 }
--- src/components/NoticePostList.jsx
+++ src/components/NoticePostList.jsx
@@ -1,23 +1,19 @@
 'use client';
 import Link from 'next/link';
-import StatusView from './BestPost/StatusView';
-import WriterView from './BestPost/WriterView';
+// import WriterView from './BestPost/WriterView';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Pagination from 'react-js-pagination';
-import Loading from '../Loading';
 import { useQuery } from '@tanstack/react-query';
+import Loading from './Loading';
+import WriterView from './Community/BestPost/WriterView';
+import StatusView from './Community/BestPost/StatusView';
 
-export default function PostList({ category }) {
+export default function NoticePostList() {
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 번호 (1부터 시작)
   const [totalPages, setTotalPages] = useState(0); // 총 페이지 수
   const [postsPerPage, setPostsPerPage] = useState(10); // 페이지당 게시글 개수
   const [totalItems, setTotalItems] = useState(0);
-  const [selectedCategory, setSelectedCategory] = useState(category);
-  useEffect(() => {
-    setSelectedCategory(category);
-    setCurrentPage(1);
-  }, [category]);
 
   const {
     data: posts,
@@ -32,11 +28,6 @@ export default function PostList({ category }) {
           page: currentPage - 1, // 페이지 번호가 0부터 시작하므로 -1
         },
       });
-      if (res.status === 200) {
-        setTotalPages(res.data.totalPages);
-        setTotalItems(res.data.totalElements);
-        setPostsPerPage(res.data.size);
-      }
       return res.data;
     },
   });
@@ -46,18 +37,6 @@ export default function PostList({ category }) {
     refetch();
   };
 
-  const filteredPosts = posts?.content?.filter((post) => {
-    switch (selectedCategory) {
-      case '자유':
-        return post.postCategory === '자유';
-      case '음식':
-        return post.postCategory === '음식';
-      case '여행지':
-        return post.postCategory === '여행지';
-      default:
-        return true;
-    }
-  });
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState(null);
   const onClickSearch = async (e) => {
@@ -69,19 +48,14 @@ export default function PostList({ category }) {
       });
       if (res.status === 200) {
         setSearchResults(res.data.content);
-        setTotalPages(res.data.totalPages);
-        setTotalItems(res.data.totalElements);
-        setPostsPerPage(res.data.size);
       }
     } catch (err) {
       console.log(err);
     }
   };
-  const displayPosts = searchResults || filteredPosts;
   if (isPending || isError) return <Loading isPending={isPending} isError={isError} />;
   return (
-    <div>
-      {/* 검색 */}
+    <div className=''>
       <div className='relative mb-4 w-1/3'>
         <input
           type='text'
@@ -95,19 +69,14 @@ export default function PostList({ category }) {
         </button>
       </div>
       <ul>
-        {displayPosts.map((data) => (
+        {posts.content.map((data) => (
           <li key={data.postId} className='flex w-full flex-col gap-4'>
             <WriterView writer={data.nickname} date={data.createAt} />
-            <div className='flex items-center gap-2 font-semibold'>
+            <div className='relative flex items-center gap-2 font-semibold'>
               <Link href={`/content/community/${data.postId}`}>{data.title}</Link>
               <span className='text-center font-semibold'>[{data.commentCount}]</span>
-            </div>
-            <div className='relative flex'>
-              <div className='w-20 rounded-md bg-pink-200 py-[2px] text-center text-sm font-semibold'>
-                {data.postCategory}
-              </div>
               <div className='absolute right-0'>
-                <StatusView viewCount={data.viewCount} likeCount={data.likeCount} />
+                <StatusView viewCount={data.viewCount} />
               </div>
             </div>
             {/* 경계선 */}
