a8ff6fd81c840981255e6603fa4205097365a625
Elbyss <elysiabyss@gmail.com>
2024-03-25 18:31:54 +0900

    👔 Update: 로그인 후 페이지 처리 수정 - 홈 화면으로 이동 -> 이전 페이지로 이동

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
         localStorage.clear();
         setToken(token);
         setMyNickname(res.data.nickname);
-        router.push('/');
+        router.back();
       }
     } catch (err) {
       console.log(err);
