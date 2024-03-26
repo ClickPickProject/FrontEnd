c6f8f77b6a138c97917d596b3e23ed40aa67cfb8
Elbyss <elysiabyss@gmail.com>
2024-03-26 17:04:35 +0900

    ♻️ Refactor: 로그인 리다이렉션 경로 수정

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
         localStorage.clear();
         setToken(token);
         setMyNickname(res.data.nickname);
-        router.back();
+        router.replace('/');
       }
     } catch (err) {
       console.log(err);
