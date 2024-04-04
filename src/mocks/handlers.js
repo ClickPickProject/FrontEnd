12ea3b260bb6ec9cb98b3c4177aa358f66073132
Elbyss <elysiabyss@gmail.com>
2024-04-04 22:46:42 +0900

    ✅ Chore: json handler 추가 - 동일 좌표 게시글 리스트 조회

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -716,11 +716,37 @@ export const handlers = [
       },
     ]);
   }),
-  http.get('/api/map/post/:xPosition/:yPosition', ({ request }) => {
-    const { xPosition, yPosition } = request.params;
+  // 동일 좌표 게시글 리스트 조회
+  http.get('/api/map/post/:xPosition/:yPosition', ({ params }) => {
+    const { xPosition, yPosition } = params;
+    console.log(xPosition, yPosition);
     return HttpResponse.json([
       {
         postId: 1,
+        nickname: '닉네임',
+        title: '제목',
+        createAt: '2024-04-01T16:00:48.884Z',
+        viewCount: 10,
+        likeCount: 10,
+        hashtags: '여행',
+        postCategory: '자유',
+        CommentCount: '30',
+        profileUrl: '/Images/forest.jpg',
+      },
+      {
+        postId: 2,
+        nickname: '닉네임',
+        title: '제목',
+        createAt: '2024-04-02T16:00:48.884Z',
+        viewCount: 10,
+        likeCount: 10,
+        hashtags: '여행',
+        postCategory: '자유',
+        CommentCount: '30',
+        profileUrl: '/Images/forest.jpg',
+      },
+      {
+        postId: 3,
         nickname: '닉네임',
         title: '제목',
         createAt: '2024-04-03T16:00:48.884Z',
