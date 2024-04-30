a41da19cc0cf04c5e58959cbd282bfa0279b3f67
Elbyss <elysiabyss@gmail.com>
2024-05-01 00:21:49 +0900

    Fix: 중복 import 제거

--- src/components/Community/PostDetail.jsx
+++ src/components/Community/PostDetail.jsx
@@ -8,9 +8,7 @@ import axios from 'axios';
 import { useParams, useRouter } from 'next/navigation';
 import { MyNicknameState, tokenState } from '@/atoms/tokenState';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { CommentIcon, EmptyHeartIcon, FillHeartIcon, ReportIcon } from '../UI/Icons';
 import { CommentIcon, EmptyHeartIcon, FillHeartIcon, ReportIcon } from '../UI/Icons';
 import Loading from '../Loading';
 import {
