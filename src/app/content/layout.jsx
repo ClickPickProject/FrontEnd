c0546d469eab9b6740aaf135cca908242d48cc9e
Elbyss <elysiabyss@gmail.com>
2024-05-01 01:22:12 +0900

    Feat: footer 수정

--- src/app/content/layout.jsx
+++ src/app/content/layout.jsx
@@ -3,6 +3,7 @@ import SideNavbar from '@/components/Community/SideNavbar';
 import { useRecoilValue } from 'recoil';
 import { pageOpacity } from '@/atoms/pageState';
 import { CallIcon, LetterIcon } from '@/components/UI/Icons';
+import Footer from '@/components/Home/Footer';
 export default function layout({ children }) {
   return (
     <>
@@ -10,16 +11,9 @@ export default function layout({ children }) {
         <SideNavbar />
         {children}
       </div>
-      <footer className='mt-12 flex h-72 w-full flex-col items-center justify-center bg-pink-100'>
-        <div className='mb-4 text-lg font-bold text-pink-800'>ClickPick Project</div>
-        <div className='mb-2 flex items-center text-pink-700'>
-          <LetterIcon className='mr-2' /> 이메일: clickpickAdmin@clickpick.kr
-        </div>
-        <div className='mb-4 flex items-center text-pink-700'>
-          <CallIcon className='mr-2' /> 전화번호: 010-456-7890
-        </div>
-        <div className='text-sm text-pink-700'>&copy; 2024 ClickPick. All rights reserved.</div>
-      </footer>
+      <div className='mt-32'>
+        <Footer />
+      </div>
     </>
   );
 }
