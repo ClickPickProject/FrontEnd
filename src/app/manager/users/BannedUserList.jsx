218170879a8a60d5d46fed6f663916d1bb5790c6
Elbyss <elysiabyss@gmail.com>
2024-05-07 18:22:37 +0900

    Feat(Manager): 기간 연장 선택 모달 추가

--- src/app/manager/users/BannedUserList.jsx
+++ src/app/manager/users/BannedUserList.jsx
@@ -3,13 +3,18 @@ import { tokenState } from '@/atoms/tokenState';
 import Loading from '@/components/Loading';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
-import { use } from 'react';
+import { useState } from 'react';
+import Select from 'react-select';
 import { toast } from 'react-toastify';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import BannedUserModal from './BannedUserModal';
+import { banPeriodModalState } from '@/atoms/commentState';
 
 export default function BannedUserList() {
   const queryClient = useQueryClient();
+  const [banPeriodUser, setBanPeriodUser] = useState('');
   const token = useRecoilValue(tokenState);
+  const [banPeriodModal, setBanPeriodModal] = useRecoilState(banPeriodModalState);
   const { data, isPending, isError } = useQuery({
     queryKey: ['banndUsers'],
     queryFn: async () => {
@@ -22,6 +27,7 @@ export default function BannedUserList() {
       return res.data;
     },
   });
+
   const onClickDelete = async (userId) => {
     try {
       const res = await axios.delete(`/api/admin/ban/${userId}`, {
@@ -38,6 +44,7 @@ export default function BannedUserList() {
       toast.error('처리 중 오류가 발생했습니다.');
     }
   };
+
   if (isPending || isError) return <Loading isPending={isPending} isError={isError} />;
   return (
     <>
@@ -47,9 +54,18 @@ export default function BannedUserList() {
             {user.id}
             <span
               onClick={() => onClickDelete(user.id)}
-              className='cursor-pointer rounded-full bg-red-500 px-2 text-white transition-all hover:scale-105 hover:bg-blue-700'
+              className='cursor-pointer rounded-full bg-red-500 px-2 text-white transition-all hover:scale-105 hover:bg-red-700'
             >
               정지 해제
+            </span>
+            <span
+              onClick={() => {
+                setBanPeriodModal(true);
+                setBanPeriodUser(user.id);
+              }}
+              className='cursor-pointer rounded-full bg-indigo-500 px-2 text-white transition-all hover:scale-105 hover:bg-indigo-700'
+            >
+              기간 변경
             </span>
           </div>
           <div className=''>{user.name}</div>
@@ -58,6 +74,22 @@ export default function BannedUserList() {
           <div className=''>{`${user.startDate.split('T')[0]} ~ ${user.endDate.split('T')[0]}`}</div>
         </div>
       ))}
+      {banPeriodModal && (
+        <>
+          <div
+            className='fixed inset-0 z-10 overflow-y-auto'
+            onKeyDown={(e) => {
+              if (e.code === 'Escape') setBanPeriodModal(false);
+            }}
+          >
+            <div className='flex min-h-screen items-center justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0'>
+              <div>
+                <BannedUserModal userId={banPeriodUser} />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
