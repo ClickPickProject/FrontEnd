9897a92117ab79436bd33ecddbb4a792d87bf9ad
Elbyss <elysiabyss@gmail.com>
2024-05-07 18:19:45 +0900

    Feat(manager): 관리자 접근 제어

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,23 +1,23 @@
 'use client';
-import { tokenState } from '@/atoms/tokenState';
+import { MyNicknameState, tokenState } from '@/atoms/tokenState';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 import { useRecoilValue } from 'recoil';
 
 const AuthContext = (WrappedComponent, options = {}) => {
   const { adminRequired = false } = options;
   const AuthComponent = (props) => {
+    const MyNickname = useRecoilValue(MyNicknameState);
     const router = useRouter();
     const token = useRecoilValue(tokenState);
     useEffect(() => {
       if (typeof window !== 'undefined') {
-        // const isAdmin = localStorage.getItem('admin') === 'true';
+        const isAdmin = MyNickname === 'ADMIN';
         if (!token) {
-          console.log('토큰 없음');
           router.push('/login');
         } else if (adminRequired && !isAdmin) {
-          // 관리자 권한 제어
-          alert('관리자만 접근할 수 있습니다.');
+          toast.error('관리자만 접근할 수 있습니다.');
           router.push('/');
         }
       }
