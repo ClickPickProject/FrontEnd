73ee0dadc7920fc73b983e8f60ecb8d069865501
Elbyss <elysiabyss@gmail.com>
2024-05-20 23:34:59 +0900

    Chore: MSW 연동 해제

--- next.config.js
+++ next.config.js
@@ -17,22 +17,7 @@ const nextConfig = {
       },
     ],
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'clickpick.iptime.org',
-        port: '8080',
-        pathname: '/profile/images/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'clickpick.iptime.org',
-        port: '8080',
-        pathname: '/post/images/**',
-      },
-    ],
-  },
+
   async headers() {
     return [
       {
@@ -50,194 +35,185 @@ const nextConfig = {
       },
     ];
   },
-  // async rewrites() {
-  //   return [
-  //     // 로그인
-  //     {
-  //       source: '/api/login',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/login`,
-  //     },
-  //     // 회원가입
-  //     {
-  //       source: '/api/signup/user',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/signup/user`,
-  //     },
-  //     // 중복확인
-  //     {
-  //       source: '/api/check/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/check/:path*`,
-  //     },
-  //     // 중복확인
-  //     {
-  //       source: '/api/verification',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/verification`,
-  //     },
+  async rewrites() {
+    return [
+      // 로그인
+      {
+        source: '/api/login',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/login`,
+      },
+      // refresh token
+      {
+        source: '/api/reissue',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/reissue`,
+      },
+      // 회원가입
+      {
+        source: '/api/signup/user',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/signup/user`,
+      },
+      // 중복확인
+      {
+        source: '/api/check/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/check/:path*`,
+      },
+      // 중복확인
+      {
+        source: '/api/verification',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/verification`,
+      },
 
-  //     // 게시글 목록, 베스트 게시글
-  //     {
-  //       source: '/api/post/list',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/post/list`,
-  //     },
+      // // 게시글 목록, 베스트 게시글
+      // {
+      //   source: '/api/post/list',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/post/list`,
+      // },
 
-  //     // 게시글 작성
-  //     {
-  //       source: '/api/member/post',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/post`,
-  //     },
+      // // 게시글 작성
+      // {
+      //   source: '/api/member/post',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/post`,
+      // },
 
-  //     // 게시글 수정
-  //     {
-  //       source: '/api/member/post/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/post/:path*`,
-  //     },
+      // // 게시글 수정
+      // {
+      //   source: '/api/member/post/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/post/:path*`,
+      // },
 
-  //     // 게시글 상세페이지
-  //     {
-  //       source: '/api/post/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/post/:path*`,
-  //     },
-  //     // 게시글 좋아요(like)
-  //     {
-  //       source: '/api/member/likedpost/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/likedpost/:path*`,
-  //     },
-  //     // 댓글 좋아요(like)
-  //     {
-  //       source: '/api/member/likedcomment/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/likedcomment/:path*`,
-  //     },
-  //     // 댓글 작성/수정/삭제
-  //     {
-  //       source: '/api/member/comment/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/comment/:path*`,
-  //     },
-  //     // 유저 정보
-  //     {
-  //       source: '/api/member/userinfo',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/userinfo`,
-  //     },
-  //     // 답글 작성
-  //     {
-  //       source: '/api/member/recomment',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/recomment`,
-  //     },
-  //     // 게시글/댓글 신고
-  //     {
-  //       source: '/api/member/report/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/report/:path*`,
-  //     },
-  //     // 유저정보 확인
-  //     {
-  //       source: '/api/member/userinfo',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/userinfo`,
-  //     },
-  //     // 회원 탈퇴
-  //     {
-  //       source: '/api/member/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/:path*`,
-  //     },
-  //     // 자신이 작성한 게시글 리스트 조회
-  //     {
-  //       source: '/api/member/post/list/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/post/list/:path*`,
-  //     },
-  //     // 자신이 작성한 댓글 리스트 조회
-  //     {
-  //       source: '/api/member/comment/list/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/comment/list/:path*`,
-  //     },
-  //     // 좋아요 한 게시글 리스트 조회
-  //     {
-  //       source: '/api/member/liked/post/list/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/liked/post/list/:path*`,
-  //     },
-  //     // 좋아요 한 댓글의 게시글 리스트 조회
-  //     {
-  //       source: '/api/member/liked/comment/list/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/liked/comment/list/:path*`,
-  //     },
-  //     // 닉네임 변경
-  //     {
-  //       source: '/api/member/new-nickname/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/new-nickname/:path*`,
-  //     },
-  //     // 전화번호 변경
-  //     {
-  //       source: '/api/member/new-phone-number/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/new-phone-number/:path*`,
-  //     },
-  //     // 프로필 사진 조회
-  //     {
-  //       source: '/api/profile/image/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/profile/image/:path*`,
-  //     }, // 프로필 사진 추가/변경/삭제
-  //     {
-  //       source: '/api/member/profile/image/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/profile/image/:path*`,
-  //     },
-  //     // 프로필 사진 조회
-  //     {
-  //       source: '/api/profile/image/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/profile/image/:path*`,
-  //     },
-  //     // 게시글 사진 추가 / 조회 / 삭제
-  //     {
-  //       source: '/api/member/post/image/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/post/image/:path*`,
-  //     },
-  //     // 지도 범위 게시글 조회
-  //     {
-  //       source: '/api/map/marker',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/map/marker`,
-  //     },
-  //     // 동일 좌표 게시글 리스트 조회
-  //     {
-  //       source: '/api/map/post/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/map/post/:path*`,
-  //     },
-  //     // 장소 즐겨찾기 / 즐겨찾기 리스트 조회
-  //     {
-  //       source: '/api/member/map/bookmark/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/map/bookmark/:path*`,
-  //     },
-  //     {
-  //       source: '/api/notice/list',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/notice/list`,
-  //     },
-  //     {
-  //       source: '/api/admin/notice',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/notice`,
-  //     },
-  //     {
-  //       source: '/api/admin/notice/:path*',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/notice/:path*`,
-  //     },
-  //     {
-  //       source: '/api/admin/userlist',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/userlist`,
-  //     },
-  //     {
-  //       source: '/api/admin/banuserlist',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/banuserlist`,
-  //     },
-  //     {
-  //       source: '/api/admin/reportpostlist',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/reportpostlist`,
-  //     },
-  //     {
-  //       source: '/api/admin/reportcommentlist',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/reportcommentlist`,
-  //     },
-  //     {
-  //       source: '/api/admin/postban',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/postban`,
-  //     },
-  //     {
-  //       source: '/api/admin/commentban',
-  //       destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/commentban`,
-  //     },
-  //   ];
-  // },
+      // 게시글 상세페이지
+      {
+        source: '/api/post/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/post/:path*`,
+      },
+      {
+        source: '/api/member/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/:path*`,
+      },
+      // // 게시글 좋아요(like)
+      // {
+      //   source: '/api/member/likedpost/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/likedpost/:path*`,
+      // },
+      // // 댓글 좋아요(like)
+      // {
+      //   source: '/api/member/likedcomment/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/likedcomment/:path*`,
+      // },
+      // // 댓글 작성/수정/삭제
+      // {
+      //   source: '/api/member/comment/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/comment/:path*`,
+      // },
+      // // 유저 정보
+      // {
+      //   source: '/api/member/userinfo',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/userinfo`,
+      // },
+      // // 답글 작성
+      // {
+      //   source: '/api/member/recomment',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/recomment`,
+      // },
+      // // 게시글/댓글 신고
+      // {
+      //   source: '/api/member/report/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/report/:path*`,
+      // },
+      // // 유저정보 확인
+      // {
+      //   source: '/api/member/userinfo',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/userinfo`,
+      // },
+      // // 회원 탈퇴
+      // {
+      //   source: '/api/member/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/:path*`,
+      // },
+      // // 자신이 작성한 게시글 리스트 조회
+      // {
+      //   source: '/api/member/post/list/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/post/list/:path*`,
+      // },
+      // // 자신이 작성한 댓글 리스트 조회
+      // {
+      //   source: '/api/member/comment/list/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/comment/list/:path*`,
+      // },
+      // // 좋아요 한 게시글 리스트 조회
+      // {
+      //   source: '/api/member/liked/post/list/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/liked/post/list/:path*`,
+      // },
+      // // 좋아요 한 댓글의 게시글 리스트 조회
+      // {
+      //   source: '/api/member/liked/comment/list/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/liked/comment/list/:path*`,
+      // },
+      // // 닉네임 변경
+      // {
+      //   source: '/api/member/new-nickname/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/new-nickname/:path*`,
+      // },
+      // 전화번호 변경
+      // {
+      //   source: '/api/member/new-phone-number/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/new-phone-number/:path*`,
+      // },
+      // 프로필 사진 조회
+      {
+        source: '/api/profile/image/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/profile/image/:path*`,
+      }, // 프로필 사진 추가/변경/삭제
+      // {
+      //   source: '/api/member/profile/image/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/profile/image/:path*`,
+      // },
+      // // 프로필 사진 조회
+      // {
+      //   source: '/api/profile/image/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/profile/image/:path*`,
+      // },
+      // // 게시글 사진 추가 / 조회 / 삭제
+      // {
+      //   source: '/api/member/post/image/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/post/image/:path*`,
+      // },
+      // 지도 범위 게시글 조회
+      {
+        source: '/api/map/marker',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/map/marker`,
+      },
+      // 동일 좌표 게시글 리스트 조회
+      {
+        source: '/api/map/post/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/map/post/:path*`,
+      },
+      // // 장소 즐겨찾기 / 즐겨찾기 리스트 조회
+      // {
+      //   source: '/api/member/map/bookmark/:path*',
+      //   destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/member/map/bookmark/:path*`,
+      // },
+      {
+        source: '/api/notice/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/notice/:path*`,
+      },
+      {
+        source: '/api/admin/withdrawal',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/withdrawal`,
+      },
+      {
+        source: '/api/admin/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/:path*`,
+      },
+
+      // 정지 기간 변경, 해제
+      {
+        source: '/api/admin/ban/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/admin/ban/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
