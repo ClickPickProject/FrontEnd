07fe501ee05bad8933685b7ff071b0903418b7b8
Elbyss <elysiabyss@gmail.com>
2024-05-21 21:01:52 +0900

    Chore: 오타 수정

--- src/components/Profile/MyProfile.jsx
+++ src/components/Profile/MyProfile.jsx
@@ -159,7 +159,6 @@ export default function MyProfile() {
         },
       });
       if (res.status === 200) {
-        setUserImg(res.data.image);
         toast.success(`이미지가 변경 되었습니다.`, {
           position: 'top-right',
         });
