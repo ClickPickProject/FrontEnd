ecaefb6d01dc4a8340e7bfe5a2fe3511dd85a96c
Elbyss <elysiabyss@gmail.com>
2024-05-28 19:41:41 +0900

    Fix: 오타 수정

--- src/app/manager/reporters/page.jsx
+++ src/app/manager/reporters/page.jsx
@@ -51,9 +51,9 @@ export default function ReportersPage() {
               <div className='bg-pink-300 text-black'>
                 <div className='grid grid-cols-8 gap-2 py-2 text-center text-sm [&>*]:font-semibold'>
                   <div className=''>#</div>
-                  <div className=''>게시글 ID</div>
                   <div className=''>신고자 ID</div>
                   <div className=''>피신고자 ID</div>
+                  <div className=''>게시글 ID</div>
                   <div className=''>사유</div>
                   <div className=''>정지 기간</div>
                   <div className=''>상태</div>
