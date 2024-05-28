9bea9a054d5edf7ba50103283b08440938b112d6
Elbyss <elysiabyss@gmail.com>
2024-05-28 19:18:01 +0900

    Chore: axios 임시 비활성화

--- src/components/utils/Axios.jsx
+++ src/components/utils/Axios.jsx
@@ -22,29 +22,28 @@ function removedHeader(key) {
   delete axiosInstance.defaults.headers.common[key];
 }
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const originalConfig = error.config;
-    if (error.response.status === 406) {
-      try {
-        console.log('406 error');
-        const res = await ReqRefreshToken();
-        console.log(res);
-        if (res.status === 200) {
-          console.log('406 error - 200');
-          originalConfig.headers.authorization = res.headers['authorization'];
-          return axiosInstance(originalConfig);
-        }
-      } catch (err) {
-        toast.error('토큰이 만료되었습니다. 다시 로그인 해주세요.');
-        removedHeader('authorization');
-      }
-    }
-    return Promise.reject(error);
-  },
-);
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const originalConfig = error.config;
+//     if (error.response.status === 406) {
+//       try {
+//         console.log('406 error');
+//         const res = await ReqRefreshToken();
+//         console.log(res);
+//         if (res.status === 200) {
+//           console.log('406 error - 200');
+//           originalConfig.headers.authorization = res.headers['authorization'];
+//           return axiosInstance(originalConfig);
+//         }
+//       } catch (err) {
+//         toast.error('토큰이 만료되었습니다. 다시 로그인 해주세요.');
+//       }
+//     }
+//     return Promise.reject(error);
+//   },
+// );
 
 export { setHeader, removedHeader };
